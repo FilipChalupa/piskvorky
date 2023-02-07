5e629137405755aa61b07e9448d62068de53d871
Filip Chalupa <chalupa.filip@gmail.com>
2023-02-08 00:41:13 +0100

    Naive AI looking for neighbours

--- src/utilities/findBestMove.ts
+++ src/utilities/findBestMove.ts
@@ -1,6 +1,48 @@
 import { findWinner as findWinnerInternal } from './findWinner'
 import { valueAt } from './valueAt'
-import { Board, empty, PlayerO, PlayerX, Position } from './values'
+import {
+	Board,
+	empty,
+	playerO,
+	PlayerO,
+	playerX,
+	PlayerX,
+	Position,
+} from './values'
+
+const adjacentDirections = [
+	{ x: 0, y: 1 },
+	{ x: 0, y: -1 },
+	{ x: 1, y: 0 },
+	{ x: -1, y: 0 },
+	{ x: 1, y: 1 },
+	{ x: 1, y: -1 },
+	{ x: -1, y: 1 },
+	{ x: -1, y: -1 },
+] as const
+
+const countAdjacent = (
+	board: Board,
+	position: Position,
+	player: PlayerO | PlayerX,
+): number => {
+	return adjacentDirections
+		.map((direction) => {
+			const value = valueAt(board, {
+				x: position.x + direction.x,
+				y: position.y + direction.y,
+			})
+			if (value === player) {
+				return 1
+			} else if (value === playerO || value === playerX) {
+				return 0.9
+			} else if (value === empty) {
+				return 0.3
+			}
+			return 0
+		})
+		.reduce((a, b) => a + b, 0)
+}
 
 export const findBestMove = (
 	board: Board,
@@ -16,14 +58,20 @@ export const findBestMove = (
 		return { x: middle, y: middle }
 	}
 
-	while (true) {
-		const x = Math.floor(Math.random() * board.length)
-		const y = Math.floor(Math.random() * board.length)
+	const neighbours = board.flatMap((row, y) =>
+		row.map((_, x) => {
+			const position = { x, y }
+			const value = valueAt(board, position)
+			const count =
+				value === empty
+					? countAdjacent(board, position, player)
+					: Number.NEGATIVE_INFINITY
 
-		const position = { x, y }
+			return { count, position }
+		}),
+	)
+	const topCount = Math.max(...neighbours.map(({ count }) => count))
+	const topPositions = neighbours.filter(({ count }) => count === topCount)
 
-		if (valueAt(board, position) === empty) {
-			return position
-		}
-	}
+	return topPositions[Math.floor(Math.random() * topPositions.length)].position
 }
